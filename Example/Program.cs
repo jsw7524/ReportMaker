2eaf5b5e6e6b805604e5196da6c6e78d08ed7194
jsw7524 <jsw7524@gmail.com>
2021-03-30 22:17:15 +0800

    Demo

---
+++ Example/Program.cs
@@ -0,0 +1,86 @@
+﻿using Example;
+using Example.Model;
+using JswTools;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class AdventureWorksManager
+    {
+        AdventureWorksLTEntities db = new AdventureWorksLTEntities();
+        public IEnumerable<Product> GetProductData()
+        {
+            return db.Product.AsEnumerable();
+        }
+
+        public void MakeReport1()
+        {
+            List<Product> Products = GetProductData().ToList();
+            DataProcessor dp = new DataProcessor();
+            dp.BeforeGroup = (x, d) =>
+             {
+                 if (1 == d)
+                 {
+                     var groupColor = x as IGrouping<string, Product>;
+                     TemplateRow tmp = new TemplateRow();
+                     tmp.RowContent.AddRange(new List<TemplateCell>() { "Color:", groupColor.Key });
+                     dp.Result.Add(tmp);
+                 }
+             };
+
+            dp.RowDetail = (x, d) =>
+            {
+                var p = x as Product;
+                TemplateRow tmp = new TemplateRow();
+                tmp.RowContent.AddRange(new List<TemplateCell>() { p.Name, p.Weight ?? 0m, p.Size });
+                dp.Result.Add(tmp);
+            };
+
+            dp.Process(Products.GroupBy(a => a.Color));
+            ReportMaker reportMaker = new ReportMaker();
+            Dictionary<string, List<TemplateRow>> dict = new Dictionary<string, List<TemplateRow>>() { { "Products", dp.Result } };
+            var image = reportMaker.FillReport("Test1.xlsx", "Template1.xlsx", dict);
+            File.WriteAllBytes("Test1.xlsx", image.ToArray());
+        }
+
+
+        public void MakeReport2()
+        {
+            List<Product> Products = GetProductData().ToList();
+            DataProcessor dp = new DataProcessor();
+            dp.BeforeGroup = (x, d) =>
+            {
+                if (1 == d)
+                {
+                    var groupColor = x as IGrouping<string, Product>;
+                    TemplateRow tmp = new TemplateRow();
+                    tmp.RowContent.AddRange(new List<TemplateCell>() { "Color:", groupColor.Key, groupColor.AsEnumerable().Count() });
+                    dp.Result.Add(tmp);
+                }
+            };
+
+            dp.Process(Products.GroupBy(a => a.Color));
+            ReportMaker reportMaker = new ReportMaker();
+            Dictionary<string, List<TemplateRow>> dict = new Dictionary<string, List<TemplateRow>>() { { "Products", dp.Result } };
+            var image = reportMaker.FillReport("Test2.xlsx", "Template1.xlsx", dict);
+            File.WriteAllBytes("Test2.xlsx", image.ToArray());
+        }
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AdventureWorksManager awm = new AdventureWorksManager();
+        awm.MakeReport1();
+        awm.MakeReport2();
+    }
+}
+
---
+++ ReportMaker/JswTools.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using OfficeOpenXml;
+using System.Data;
+
+
+namespace JswTools
+{
+    public class ToolsJsw
+    {
+        public string ToYYYMMDD(int yyyy, int mm, int dd)
+        {
+            return (yyyy - 1911).ToString() + "." + mm.ToString() + "." + dd.ToString();
+        }
+    }
+    public class ReportMaker
+    {
+        Dictionary<string, ExcelRange> stylesDict = new Dictionary<string, ExcelRange>();
+        public MemoryStream FillReport(string filename, string templatefilename, Dictionary<string, List<TemplateRow>> data)
+        {
+            return FillReport(filename, templatefilename, data, new string[] { "%", "#" });
+        }
+        public MemoryStream FillReport(string filename, string templatefilename, Dictionary<string, List<TemplateRow>> data, string[] deliminator)
+        {
+            MemoryStream file = new MemoryStream();
+            using (FileStream temp = new FileStream(templatefilename, FileMode.Open))
+            {
+                using (ExcelPackage xls = new ExcelPackage(file, temp))
+                {
+                    foreach (var ws in xls.Workbook.Worksheets)
+                    {
+                        if ("StylesJSW" == ws.Name)
+                            continue;
+                        foreach (var c in ws.Cells)
+                        {
+                            string dataLabel = "" + c.Value;
+                            if (dataLabel.StartsWith(deliminator[0]) == false || dataLabel.EndsWith(deliminator[1]) == false)
+                            {
+                                continue;
+                            }
+                            dataLabel = dataLabel.Replace(deliminator[0], "").Replace(deliminator[1], "");
+                            try
+                            {
+                                for (int row = 0; row < data[dataLabel].Count; row++)
+                                {
+                                    var rowData = data[dataLabel][row];
+                                    for (int col = 0; col < rowData.RowContent.Count ; col++)
+                                    {
+                                        if ("" == rowData.RowContent[col].info)
+                                            continue;
+                                        xls.Workbook.Worksheets["StylesJSW"].Cells[rowData.RowContent[col].info].Copy(ws.Cells[row+c.Start.Row, col+c.Start.Column]);
+                                    }
+                                }
+                                var reportData = ((data[dataLabel]).Select(a => a.RowContent).Select(b => b.Select(z => z.content as object).ToArray()).ToArray());
+                                c.LoadFromArrays(reportData);
+                            }
+                            catch (Exception ex)
+                            {
+                                c.Value = dataLabel + " not found";
+                            }
+                        }
+                    }
+                    if (null != xls.Workbook.Worksheets["StylesJSW"])
+                    {
+                        xls.Workbook.Worksheets.Delete("StylesJSW");
+                    }
+                    xls.Save();
+                }
+            }
+            return file;
+        }
+    }
+    public class TemplateCell
+    {
+        public static implicit operator TemplateCell(Decimal dec)
+        {
+            return new TemplateCell(dec);
+        }
+        public static implicit operator TemplateCell(string str)
+        {
+            return new TemplateCell(str);
+        }
+        public TemplateCell(object c) : this(c, "")
+        {
+            return;
+        }
+        public TemplateCell(object c, string t)
+        {
+            content = c;
+            info = t;
+        }
+        public object content;
+        public string info;
+    }
+
+    public class TemplateRow : IEnumerable
+    {
+        public List<TemplateCell> RowContent { set; get; }
+        public string RowStyle { set; get; }
+        public bool NewPage { set; get; }
+        public TemplateRow()
+        {
+            RowContent = new List<TemplateCell>();
+            RowStyle = "";
+            NewPage = false;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return RowContent.GetEnumerator();
+        }
+    }
+
+    public class DataProcessor
+    {
+        public List<TemplateRow> Result { get; set; }
+        public Action<IEnumerable, int> BeforeGroup = (x, d) => { };
+        public Action<IEnumerable, int> AfterGroup = (x, d) => { };
+        public Action<object, int> RowDetail = (x, d) => { };
+        public Action<IEnumerable> MakeHead = x => { };
+        public Action<IEnumerable> MakeFoot = x => { };
+
+        public DataProcessor()
+        {
+            Result = new List<TemplateRow>();
+        }
+
+        public void Process(IEnumerable data)
+        {
+            MakeHead(data);
+            MakeBody(data, 0);
+            MakeFoot(data);
+        }
+
+        public void MakeBody(IEnumerable data, int level)
+        {
+            foreach (var d in data)
+            {
+                if (d is IEnumerable)
+                {
+                    BeforeGroup(d as IEnumerable, level + 1);
+                    MakeBody(d as IEnumerable, level + 1);
+                    AfterGroup(d as IEnumerable, level + 1);
+                }
+                else
+                {
+                    RowDetail(d, level);
+                }
+            }
+        }
+    }
+}
