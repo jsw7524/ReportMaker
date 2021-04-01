7bd470a4a56d5117dbab763237d8fafe8ec23a4c
jsw7524 <jsw7524@gmail.com>
2021-04-01 23:05:04 +0800

    DataProcessor

---
+++ ReportMaker/ReportMakerHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace JswTools
+{
+    public class ReportMakerHelper
+    {
+        public string ToROCYearMMDD(int yyyy, int mm, int dd)
+        {
+            return (yyyy - 1911).ToString() + "." + mm.ToString() + "." + dd.ToString();
+        }
+    }
+}
---
+++ ReportMaker/TemplateRow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace JswTools
+{
+    public class TemplateRow : IEnumerable
+    {
+        public List<TemplateCell> RowContent { set; get; }
+        public string RowStyle { set; get; }
+        public bool NewPage { set; get; }
+        public TemplateRow()
+        {
+            RowContent = new List<TemplateCell>();
+            RowStyle = "";
+            NewPage = false;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return RowContent.GetEnumerator();
+        }
+    }
+}
